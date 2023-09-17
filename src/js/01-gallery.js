71c2e2e5e4ac64e193978329ca225e547929f3ed
Olena Roganova <132047130+olenaroganova@users.noreply.github.com>
2023-09-17 22:16:04 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+let lightbox;
+
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+list.addEventListener('click', handleClick);
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+            <img src="${preview}" alt="${description}" />
+        </a>`).join('');
+}
+
+function handleClick(event) {
+    event.preventDefault();
+    
+    if (!event.target.classList.contains('gallery__item')) {
+        return;
+    }
+
+    if (lightbox) {
+        lightbox.destroy();
+    }
+
+    lightbox = new SimpleLightbox(event.target.parentElement, {
+        captionsData: "alt",
+        captionPosition: "bottom",
+    });
+
+    lightbox.show();
+    
+    window.addEventListener('keyup', closeModal);
+}
+
+function closeModal(event) {
+    if (event.key === "Escape" && lightbox) {
+        lightbox.close();
+        window.removeEventListener('keyup', closeModal);
+    }
+}
